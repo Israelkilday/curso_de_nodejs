f8fb2b9028f74322d2c3d94dcba15ed7baef0b2a
Israel Kilday <israel.kilday@yahoo.com.br>
2024-03-21 23:24:37 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+});
+
+app.get('/user/:nome/sobrenome/:sobrenome', function (req, res) {
+  res.send({
+    name: req.params.nome,
+    sobrenome: req.params.sobrenome,
+  });
+});
+
+app.listen(8080, function () {
+  console.log('Servidor rodando na porta 8080');
+});
