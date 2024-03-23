75ee1404e5b435572d23bfb56b467fcb8c9f4570
Israel Kilday <israel.kilday@yahoo.com.br>
2024-03-22 22:05:46 -0300

    feat: insert automatic router

---
+++ src/routerLoader.js
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export const routerLouder = (app) => {
+  const modulesPath = path.join(__dirname, 'modules');
+
+  fs.readdirSync(modulesPath).forEach(async (dir) => {
+    const modulePath = path.join(modulesPath, dir);
+
+    if (fs.statSync(modulePath).isDirectory()) {
+      const controllerPath = path.join(modulePath, `${dir}.controller.js`);
+      if (fs.existsSync(controllerPath)) {
+        // const controller = await import(controllerPath);
+        const controller = await import(`./modules/${dir}/${dir}.controller.js`);
+
+        if (controller.default && typeof controller.default === 'function') {
+          app.use(controller.default);
+        }
+      }
+    }
+  });
+};
