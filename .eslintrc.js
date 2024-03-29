306404a5fa4acd245ac6c32fca30224e3a223f4a
Israel Kilday <israel.kilday@yahoo.com.br>
2024-03-28 21:03:55 -0300

    feat: creat exceptions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,28 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  // 'plugin:prettier/recommended'
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: ['plugin:@typescript-eslint/recommended',  ,'prettier'],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-inferrable-types': 'error',
+  },
+};
+
 // {
 //     "extends": [
 //         "eslint:recommended",
@@ -48,47 +73,46 @@
 //   }
 // }
 
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'error',
-    '@typescript-eslint/explicit-module-boundary-types': 'error',
-    '@typescript-eslint/no-explicit-any': 'error',
-    '@typescript-eslint/no-inferrable-types': 'error',
-    'sort-imports': [
-      'error',
-      {
-        ignoreCase: true,
-        ignoreDeclarationSort: true,
-        ignoreMemberSort: false,
-      },
-    ],
-    // 'import/order': [
-    //   'error',
-    //   {
-    //     groups: ['builtin', 'external', 'internal'],
-    //     pathGroups: [
-    //       {
-    //         pattern: 'react',
-    //         group: 'external',
-    //         position: 'before',
-    //       },
-    //     ],
-    //   },
-    // ],
-  },
-};
+// 'sort-imports': [
+//   'error',
+//   {
+//     ignoreCase: true,
+//     ignoreDeclarationSort: true,
+//     ignoreMemberSort: false,
+//   },
+// ],
+// 'import/order': [
+//   'error',
+//   {
+//     groups: ['builtin', 'external', 'internal'],
+//     pathGroups: [
+//       {
+//         pattern: 'react',
+//         group: 'external',
+//         position: 'before',
+//       },
+//     ],
+//   },
+// ],
+
+// 'sort-imports': [
+//   'error',
+//   {
+//     ignoreCase: true,
+//     ignoreDeclarationSort: true,
+//     ignoreMemberSort: false,
+//   },
+// ],
+// 'import/order': [
+//   'error',
+//   {
+//     groups: ['builtin', 'external', 'internal'],
+//     pathGroups: [
+//       {
+//         pattern: 'react',
+//         group: 'external',
+//         position: 'before',
+//       },
+//     ],
+//   },
+// ],
---
+++ src/exceptions/bad-request-exception.ts
@@ -0,0 +1,7 @@
+import { AppException } from './app-exception';
+
+export class BadrequestException extends AppException {
+  constructor(message: string) {
+    super(message);
+  }
+}
