fdd15a5dec0be860df843d4e8f004a6ca895386a
Israel Kilday <israel.kilday@yahoo.com.br>
2024-04-19 14:13:12 -0300

    feat: insert validation token

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,40 +1,9 @@
-// import { Request, Response, Router } from 'express';
-// import { createUser, getUsers } from './user.servivce';
-// import { UseInsertDTO } from './dtos/user-insert.dto';
-// // import { NotFoundException } from '@exceptions/not-found-exception';
-// import { ReturnError } from '@exceptions/dtos/return-error.dto';
-
-// const userRouter = Router();
-
-// const router = Router();
-
-// userRouter.use('/user', router);
-
-// router.get('/', async (_, res: Response): Promise<void> => {
-//   const users = await getUsers().catch((error) => {
-//     new ReturnError(res, error);
-//   });
-//   res.send(users);
-// });
-
-// router.post(
-//   '/',
-//   async (req: Request<undefined, undefined, UseInsertDTO>, res: Response): Promise<void> => {
-//     const user = await createUser(req.body).catch((error) => {
-//       new ReturnError(res, error);
-//     });
-//     res.send(user);
-//   },
-// );
-
-// export default userRouter;
-
 import { Request, Response, Router } from 'express';
 import { createUser, getUsers } from './user.servivce';
 import { UseInsertDTO } from './dtos/user-insert.dto';
-// import { NotFoundException } from '@exceptions/not-found-exception';
 import { ReturnError } from '@exceptions/dtos/return-error.dto';
 import { NotFoundException } from '@exceptions/not-found-exception';
+import { verifyToken } from '@utils/auth';
 
 const userRouter = Router();
 
@@ -42,7 +11,13 @@ const router = Router();
 
 userRouter.use('/user', router);
 
-router.get('/', async (_, res: Response): Promise<void> => {
+router.get('/', async (req: Request, res: Response): Promise<void> => {
+  const auhtorization = req.headers.authorization;
+
+  verifyToken(auhtorization).catch((error) => {
+    new ReturnError(res, error);
+  });
+
   const users = await getUsers().catch((error) => {
     if (error instanceof NotFoundException) {
       res.status(204);
