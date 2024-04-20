f6743b30f6d9fd41ca32fb2b1bba306254e19b85
Israel Kilday <israel.kilday@yahoo.com.br>
2024-04-19 22:30:28 -0300

    feat: change order in controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,36 +3,20 @@ import { AuthDTO } from './dtos/auth.dto';
 import { validateAuth } from './auth.service';
 import { ReturnError } from '@exceptions/dtos/return-error.dto';
 
-const authRouter = Router();
+const auth = async (req: Request<undefined, undefined, AuthDTO>, res: Response): Promise<void> => {
+  const user = await validateAuth(req.body).catch((error) => {
+    new ReturnError(res, error);
+  });
 
+  res.send(user);
+};
+
+const authRouter = Router();
 const router = Router();
 
 authRouter.use('/auth', router);
 
-router.post(
-  '/',
-  async (req: Request<undefined, undefined, AuthDTO>, res: Response): Promise<void> => {
-    const user = await validateAuth(req.body).catch((error) => {
-      new ReturnError(res, error);
-    });
-
-    res.send(user);
-  },
-);
-
-// router.post(
-//   '/',
-//   async (req: Request<undefined, undefined, AuthDTO>, res: Response): Promise<void> => {
-//     try {
-//       const user = await validateAuth(req.body);
-//       res.send(user);
-//     } catch (error) {
-//       const returnError = new ReturnError(error);
-//       returnError.sendResponse(res);
-//       return;
-//     }
-//   },
-// );
+router.post('/', auth);
 
 export default authRouter;
 
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -5,25 +5,17 @@ import { ReturnError } from '@exceptions/dtos/return-error.dto';
 import { NotFoundException } from '@exceptions/not-found-exception';
 import { authMiddleware } from '@middlewares/auth.middlaware';
 
-const userRouter = Router();
+const createUserController = async (
+  req: Request<undefined, undefined, UseInsertDTO>,
+  res: Response,
+): Promise<void> => {
+  const user = await createUser(req.body).catch((error) => {
+    new ReturnError(res, error);
+  });
+  res.send(user);
+};
 
-const router = Router();
-
-userRouter.use('/user', router);
-
-router.post(
-  '/',
-  async (req: Request<undefined, undefined, UseInsertDTO>, res: Response): Promise<void> => {
-    const user = await createUser(req.body).catch((error) => {
-      new ReturnError(res, error);
-    });
-    res.send(user);
-  },
-);
-
-router.use(authMiddleware);
-
-router.get('/', async (req: Request, res: Response): Promise<void> => {
+const getUsersController = async (req: Request, res: Response): Promise<void> => {
   const users = await getUsers().catch((error) => {
     if (error instanceof NotFoundException) {
       res.status(204);
@@ -32,6 +24,15 @@ router.get('/', async (req: Request, res: Response): Promise<void> => {
     }
   });
   res.send(users);
-});
+};
+
+const userRouter = Router();
+const router = Router();
+
+userRouter.use('/user', router);
+
+router.post('/', createUserController);
+router.use(authMiddleware);
+router.get('/', getUsersController);
 
 export default userRouter;
